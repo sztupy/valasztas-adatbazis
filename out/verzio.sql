0482263ec12e565814e00d835a7faf79b64f47b8
Peter Gyongyosi <gypeter@gmail.com>
2018-04-10 18:38:41 +0200

    added CSV output, too

--- out/verzio.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO verzio (ver,tjel,eng1,eng2,feldar,levell,levszl) VALUES
-('2018.04.09.16:53:43',3,1,1,'98.96','96.04',100);
---
+++ sql/hatarszamf.sql
@@ -0,0 +1,2 @@
+INSERT INTO hatarszamf (hszav1,hszav2,hszav3,hszav4) VALUES
+(266269,532538,798807,22424);
